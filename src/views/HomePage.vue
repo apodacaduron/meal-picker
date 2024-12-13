63337e5898d80fcc813da03d787c0bcb51c845cc
Daniel Apodaca <apodacaduron@gmail.com>
2024-12-13 17:03:09 -0600

    add basic meals list

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,7 +2,12 @@
   <ion-page>
     <ion-header :translucent="true">
       <ion-toolbar>
-        <ion-title>Blank</ion-title>
+        <ion-title>Meal Picker</ion-title>
+        <ion-buttons slot="primary">
+          <ion-button>
+            Pick random
+          </ion-button>
+        </ion-buttons>
       </ion-toolbar>
     </ion-header>
 
@@ -10,47 +15,44 @@
       <ion-header collapse="condense">
         <ion-toolbar>
           <ion-title size="large">Blank</ion-title>
+          <ion-button fill="clear">Clear</ion-button>
         </ion-toolbar>
       </ion-header>
 
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-      </div>
+      <ion-content class="ion-padding">
+        <ion-item v-for="(meal, index) in meals" :key="index">
+          <ion-avatar slot="start">
+            <div class="emoji">{{ meal.icon }}</div>
+          </ion-avatar>
+          <div class="label">
+            <ion-label>{{ meal.title }}</ion-label>
+          </div>
+        </ion-item>
+      </ion-content>
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import meals from '@/data/meals.json'
+
+function pickRandomMeal() {
+  const randomMeal = meals[Math.floor(Math.random() * meals.length)];
+
+  return randomMeal;
+}
 </script>
 
 <style scoped>
-#container {
-  text-align: center;
-  
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
+.emoji {
+  font-size: 24px;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
-
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
-}
-
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
-  margin: 0;
-}
-
-#container a {
-  text-decoration: none;
+.label {
+  display: flex;
+  flex-direction: column;
 }
 </style>
